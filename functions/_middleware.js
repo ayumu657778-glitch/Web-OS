e2c581bd28ea5167b1fa3d07f82698e9497d12fb
ayumu657778-glitch <ayumu657778@gmail.com>
2026-07-25 16:44:54 +0900

    Update _middleware.js

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -1,18 +1,3 @@
-export async function onRequest(context) {
-
-  const url = new URL(context.request.url);
-
-  if (url.pathname.includes("仮想os.html")) {
-
-    const cookie = context.request.headers.get("Cookie") || "";
-
-    if (!cookie.includes("access=yes")) {
-      return Response.redirect(
-        new URL("/index.html", url.origin),
-        302
-      );
-    }
-  }
-
-  return context.next();
+export async function onRequest() {
+  return new Response("Functionsは動いています");
 }
